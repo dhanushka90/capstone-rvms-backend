4dabd22fa0fac634eb7ed2100695529b4c781cb8
dhanushka90 <66157527+dhanushka90@users.noreply.github.com>
2023-04-13 21:27:58 -0400

    adding del, put and by id

--- capstone-backend-rvms/src/main/java/com/dksoft/springboot/controller/JourneyRefrigeratorController.java
+++ capstone-backend-rvms/src/main/java/com/dksoft/springboot/controller/JourneyRefrigeratorController.java
@@ -1,15 +1,22 @@
 package com.dksoft.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dksoft.springboot.exception.ResourceNotFoundException;
 import com.dksoft.springboot.model.JourneyRefrigerator;
 import com.dksoft.springboot.repository.JourneyRefrigeratorRepository;
 
@@ -22,7 +29,7 @@ public class JourneyRefrigeratorController {
 	private JourneyRefrigeratorRepository journeyRefrigeratorRepository;
 	
 	//get all journey and refrigerator data
-	@GetMapping("/allJourneyRefrigeratorData")
+	@GetMapping("/journeyRefrigerator")
 	public List<JourneyRefrigerator> getAllJourneyRefrigeratorData(){
 		return journeyRefrigeratorRepository.findAll();
 	}
@@ -32,6 +39,48 @@ public class JourneyRefrigeratorController {
 	public JourneyRefrigerator createJourneyRefrigerator(@RequestBody JourneyRefrigerator journeyrefrigerator) {
 		return journeyRefrigeratorRepository.save(journeyrefrigerator);
 	}
+	
+	// get journey and refrigerator by id
+	@GetMapping("/journeyRefrigerator/{id}")
+	public ResponseEntity<JourneyRefrigerator> getJourneyRefrigeratorById(@PathVariable Long id) {
 		
+		JourneyRefrigerator journeyRefrigerator = journeyRefrigeratorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no journey or refrigerator exist with id:" +id));
+		
+		return ResponseEntity.ok(journeyRefrigerator);
+	}
+	
+	// update journey and refrigerator
+	@PutMapping("/journeyRefrigerator/{id}")
+	public ResponseEntity<JourneyRefrigerator> updateJourneyRefrigerator(@PathVariable Long id, @RequestBody JourneyRefrigerator journeyRefrigeratorDetails){
+		
+		JourneyRefrigerator journeyRefrigerator = journeyRefrigeratorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no journey or refrigerator exist with id:" +id));
+		
+		journeyRefrigerator.setDeviceId(journeyRefrigeratorDetails.getDeviceId());	
+		journeyRefrigerator.setTransitOrInhouse(journeyRefrigeratorDetails.isTransitOrInhouse());
+		journeyRefrigerator.setJourneyOrRefrigeratorName(journeyRefrigeratorDetails.getJourneyOrRefrigeratorName());
+		journeyRefrigerator.setStartTime(journeyRefrigeratorDetails.getStartTime());
+		journeyRefrigerator.setStatus(journeyRefrigeratorDetails.isStatus());
+		journeyRefrigerator.setTempHigh(journeyRefrigeratorDetails.getTempHigh());
+		journeyRefrigerator.setTempLow(journeyRefrigeratorDetails.getTempLow());
+		journeyRefrigerator.setUserId(journeyRefrigeratorDetails.getUserId());
+		
+		JourneyRefrigerator updatedJourneyRefrigerator = journeyRefrigeratorRepository.save(journeyRefrigerator);
+		return ResponseEntity.ok(updatedJourneyRefrigerator);
+	}	
+	
+	// delete ticket
+	@DeleteMapping("/journeyRefrigerator/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteJourneyRefrigerator(@PathVariable Long id) {
+		
+		JourneyRefrigerator journeyRefrigerator = journeyRefrigeratorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no journey or refrigerator exist with id:" +id));
+		
+		journeyRefrigeratorRepository.delete(journeyRefrigerator);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
 
 }
--- capstone-backend-rvms/src/main/java/com/dksoft/springboot/controller/UserController.java
+++ capstone-backend-rvms/src/main/java/com/dksoft/springboot/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.dksoft.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.dksoft.springboot.exception.ResourceNotFoundException;
 import com.dksoft.springboot.model.User;
 import com.dksoft.springboot.repository.UserRepository;
 
@@ -22,7 +28,7 @@ public class UserController {
 	private UserRepository userRepository;
 	
 	//get all user data
-	@GetMapping("/allUserData")
+	@GetMapping("/users")
 	public List<User> getAllUserData(){
 		return userRepository.findAll();
 	}
@@ -32,4 +38,50 @@ public class UserController {
 	public User createUser(@RequestBody User user) {
 		return userRepository.save(user);
 	}
+	
+	// get user by id
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id) {
+		
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no user exist with id:" +id));
+		
+		return ResponseEntity.ok(user);
+	}
+	
+
+	// update user
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails){
+		
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no user exist with id:" +id));
+		
+		user.setUserName(userDetails.getUserName());	
+		user.setPassword(userDetails.getPassword());
+		user.setFirstName(userDetails.getFirstName());
+		user.setLastName(userDetails.getLastName());
+		user.setEmail(userDetails.getEmail());
+		user.setMobile(userDetails.getMobile());
+
+		
+		User updatedUser = userRepository.save(user);
+		return ResponseEntity.ok(updatedUser);
+	}	
+	
+	
+	// delete user
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id) {
+		
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("There is no user exist with id:" +id));
+		
+		userRepository.delete(user);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+	
+	
 }
